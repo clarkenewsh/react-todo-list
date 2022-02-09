067e8b56caf5bd6ec3eeebcbfcbf97f21a196d9b
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-09 13:38:53 +0000

    problem / solution breakdown in to task list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 
 import './App.css';
 
+// 1 - Form component using controlled inputs and onSubmitHandler function to submit a new to do
+// 2 - ToDoList component which will map over todo array and output each todo within the ToDoItem component
+// 3 - ToDoitem to display single to do items
+// 4 - Component t helpers functions - handleSubmit, handleOnChange, handleDelete (from array index), handleComplete
+// 5 - useState to store the list of todo within array
+// 6 - Spread operator to update the state of the todo array
+// 7 - Conditional rendering 
+// 8 - event handlers - Form = onChange & onSubmit, Onclick (delete & complete)
+
 function App() {
+
+
   return (
     <div>
+
     </div>
   )
 }
