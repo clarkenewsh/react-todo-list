9c5d27da208c6a2cc6ff38e3346966e232111179
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-11 13:42:52 +0000

    reacting testing libray unit testing for submit & delete btn & displaying todo items

---
+++ src/__test__/App.test.js
@@ -0,0 +1,67 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+// app startup
+test('app renders header at startup', () => {
+  render(<App />);
+  const element = screen.getByText("All Tasks");
+  expect(element).toBeInTheDocument();
+});
+
+
+describe('input element', () => {
+  // Input element displayed
+  test("input element is displayed", () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText('Add new todo');
+    expect(input).toBeInTheDocument();
+  });
+
+  // user input text displayed when adding task
+  test("input text is displayed", () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText('Add new todo');
+    // target the input value text added by user - i.e target.value
+    fireEvent.change(input, { target: {value: "get food"}})
+    expect(input.value).toBe("get food");
+  });
+
+})
+
+
+// Submit task button tests
+describe("submit button", () => {
+  // submission button adds task (fireEvent) 
+  test("renders submit", () => {
+    render(<App />);
+    const button = screen.getByText('Submit');
+    expect(button).toBeInTheDocument();
+  });
+
+   // submission button adds task (fireEvent) 
+   test("submit button adds task", () => {
+    render(<App />);
+    const button = screen.getByText('Submit');
+    fireEvent.click(button);
+    const deleteButton = screen.getByText('delete');
+    expect(deleteButton).toBeInTheDocument();
+  });
+    // submission button adds & displays multiple tasks (fireEvent) 
+    test("submit button adds & displays multiple tasks", () => {
+    render(<App />);
+    const button = screen.getByText('Submit');
+    fireEvent.click(button);
+    fireEvent.click(button);
+    const deleteButton = screen.getAllByText('delete');
+    expect(deleteButton.length).toBe(2);
+  });
+})
+
+
+// Delete button tests
+describe('delete button', () => {
+  // deletion of an item removed the element so it is no longer displayed to the user
+  test('')
+})
+
+
